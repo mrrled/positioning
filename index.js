f20e9420dd0a228767feda619e5acab469118aaf
Applopie <stina.nekrasova.2@yandex.ru>
2026-04-07 18:50:10 +0500

    added 5

--- index.js
+++ index.js
@@ -6,24 +6,25 @@
     element.style.width = '300px';
 */
 const progressBar = document.querySelector('.progress-bar');
-let progress = 0;
+const overlay = document.querySelector('.overlay');
 
-const interval = setInterval(() => {
-    progress += (1/3);
-    if (progress >= 100) {
-        progressBar.style.width = `100%`;
-        clearInterval(interval);
-    } else {
-        progressBar.style.width = `${progress}%`;
-    }
-}, 10);
-function open(){
-    alert("Hello World!");
+function startAnimation() {
+    let progress = 0;
+
+    const interval = setInterval(() => {
+        progress += (1/3);
+        if (progress >= 100) {
+            progressBar.style.width = `100%`;
+            clearInterval(interval);
+        } else {
+            progressBar.style.width = `${progress}%`;
+        }
+    }, 10);
 }
 
-const overlay = document.querySelector('.overlay');
 function openWindow() {
     overlay.classList.add('active');
+    startAnimation();
 }
 function closeWindow() {
     overlay.classList.remove('active');
