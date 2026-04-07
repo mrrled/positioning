609cc6e65b8d9cf5c05a06b052179964c7249388
Applopie <stina.nekrasova.2@yandex.ru>
2026-04-07 18:30:53 +0500

    progress bar and something else

--- index.js
+++ index.js
@@ -5,6 +5,26 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const progressBar = document.querySelector('.progress-bar');
+let progress = 0;
+
+const interval = setInterval(() => {
+    progress += (1/3);
+    if (progress >= 100) {
+        progressBar.style.width = `100%`;
+        clearInterval(interval);
+    } else {
+        progressBar.style.width = `${progress}%`;
+    }
+}, 10);
 function open(){
     alert("Hello World!");
+}
+
+const overlay = document.querySelector('.overlay');
+function openWindow() {
+    overlay.classList.add('active');
+}
+function closeWindow() {
+    overlay.classList.remove('active');
 }
